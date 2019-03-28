0ae4183f177b0832f34d9bf6f1f2fcb2eaa884cd
orix1944 <bf109.sai@gmail.com>
2019-03-28 14:41:13 +0900

    Change to son

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,55 @@
+$(function(){
+  function buildHTML(message){
+    if(message.text != 0){
+      var xxx = `<p class="lower-message__content">
+        ${message.text}
+      </p>`
+    }
+    else{
+      var xxx = `<img src= ${message.image} class: 'lower-message__image'>`
+    }
+    var html = `<div class="message">
+  <div class="upper-message">
+    <div class="upper-message__user-name">
+      ${message.user_name}
+    </div>
+    <div class="upper-message__date">
+      ${message.created_at}
+    </div>
+  </div>
+  <div class="lower-message">
+    ${xxx}
+
+
+
+  </div>
+</div>
+`
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main-messages').append(html);
+      $('.main-messages').animate({scrollTop: $('.main-messages')[0].scrollHeight}, 'fast');
+      $('.form__submit').attr('disabled', false);
+      $('.textbox').val('')
+     })
+    .fail(function(){
+      alert('非同期通信に失敗しました');
+    })
+    })
+  });
+
