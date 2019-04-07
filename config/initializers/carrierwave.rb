84643e3381120f94951de4aa71028555c29ba7d6
orix1944 <bf109.sai@gmail.com>
2019-04-07 14:43:04 +0900

    ”add-carriewave.rb”

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'image-bottum'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/image-bottum'
 end
