5b2dad72ebf1f97ddeb13955850017e324c8ef7b
orix1944 <bf109.sai@gmail.com>
2019-04-12 20:56:00 +0900

    ”change-message-json”

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,13 +1,23 @@
 $(function(){
   function buildHTML(message){
-    if(message.text != 0){
+     if(message.text != 0 && message.image != ""){
+      var content = `<p class="lower-message__content">
+        ${message.text}
+      </p>
+        <img src= "${message.image}" class: 'lower-message__image'>`
+    }
+
+    else if(message.text != 0){
       var content = `<p class="lower-message__content">
         ${message.text}
       </p>`
     }
+
     else{
       var content = `<img src= "${message.image}" class: 'lower-message__image'>`
     }
+
+
     var html = `<div class="message" data-id="${message.id}">
   <div class="upper-message">
     <div class="upper-message__user-name">
@@ -25,58 +35,65 @@ $(function(){
 `
     return html;
   }
-
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
+  function update(){
+    console.log('hey')
+    var message_id = $('.message:last').data('id') || 0;
     $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
+      type: 'GET',
+      url: location.href,
+      data: { id: message_id },
       dataType: 'json',
-      processData: false,
-      contentType: false
     })
+
     .done(function(data){
-      var html = buildHTML(data);
-      $('.main-messages').append(html);
-      $('.main-messages').animate({scrollTop: $('.main-messages')[0].scrollHeight}, 'fast');
-      $('.form__submit').attr('disabled', false);
-      $('#new_message')[0].reset();
-      $('.textbox').val('')
+      console.log('aa')
+       if (data.length != null){
+       data.forEach(function(message) {
+        var html = buildHTML(message);
+        $('.main-messages').append(html);
+        $('.main-messages').animate({scrollTop: $('.main-messages')[0].scrollHeight}, 'fast');
+      })
+      }
      })
     .fail(function(){
       alert('非同期通信に失敗しました');
     })
-    })
+   }
 
-    function update(){
-      console.log('hey')
-      var message_id = $('.message:last').data('id') || 0;
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    if ( $('.form__message').val() == "" && $(".hidden").val() == ""){
+      alert('何か入力してください')
+    return false;
+    }
+
+    else {
+      var url = $(this).attr('action')
       $.ajax({
-        type: 'GET',
-        url: location.href,
-        data: { id: message_id },
+        url: url,
+        type: "POST",
+        data: formData,
         dataType: 'json',
-      })
+        processData: false,
+        contentType: false
+       })
       .done(function(data){
-         if (data.length != null){
-         data.forEach(function(message) {
-          var html = buildHTML(message);
-          $('.main-messages').append(html);
-          $('.main-messages').animate({scrollTop: $('.main-messages')[0].scrollHeight}, 'fast');
-        })
-        }
+        var html = buildHTML(data);
+        $('.main-messages').append(html);
+        $('.main-messages').animate({scrollTop: $('.main-messages')[0].scrollHeight}, 'fast');
+        $('.form__submit').attr('disabled', false);
+        $('#new_message')[0].reset();
+        $('.textbox').val('')
        })
       .fail(function(){
         alert('非同期通信に失敗しました');
       })
-     }
-
-       var number = $('.main-content').data('group-id');
-       console.log(location.pathname)
-     if (location.pathname == `/groups/${number}/messages`) {
-      setInterval(update,5000);
-     }
+    }
   });
+  var number = $('.main-content').data('group-id');
+  console.log(location.pathname)
+  if (location.pathname == `/groups/${number}/messages`) {
+    setInterval(update,5000);
+  }
+});
